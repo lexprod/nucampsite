efe0291bc8c56dff159e377d1c59d056ae1791b2
Alex Colby <lexprod@gmail.com>
2023-09-20 15:25:21 -0700

    redux

--- features/campsites/campsitesSlice.js
+++ features/campsites/campsitesSlice.js
@@ -1,67 +1,41 @@
-// import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { baseUrl } from '../../app/shared/baseUrl';
-import { mapImageURL } from '../../utils/mapImageURL';
+import { baseUrl } from '../../shared/baseUrl';
 
-//create async thunk has its own error handling
 export const fetchCampsites = createAsyncThunk(
     'campsites/fetchCampsites',
     async () => {
         const response = await fetch(baseUrl + 'campsites');
         if (!response.ok) {
-            return Promise.reject('Unable to fetch, status: ' + response.status);
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
         }
         const data = await response.json();
         return data;
     }
 );
 
-
-const initialState = {
-    campsitesArray: [],
-    isLoading: true,
-    errMsg: ''
-};
-
 const campsitesSlice = createSlice({
     name: 'campsites',
-    initialState,
+    initialState: { isLoading: true, errMess: null, campsitesArray: [] },
     reducers: {},
-    extraReducers: {
-        [fetchCampsites.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [fetchCampsites.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.errMsg = '';
-            state.campsitesArray = mapImageURL(action.payload);
-        },
-        [fetchCampsites.rejected]: (state, action) => {
-            state.isLoading = false;
-            state.errMsg = action.error ? action.error.message : 'Fetch failed';
-        }
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCampsites.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchCampsites.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.errMess = null;
+                state.campsitesArray = action.payload;
+            })
+            .addCase(fetchCampsites.rejected, (state, action) => {
+                state.isLoading = false;
+                state.errMess = action.error
+                    ? action.error.message
+                    : 'Fetch failed';
+            });
     }
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
-
-export const selectAllCampsites = (state) => {
-    return state.campsites.campsitesArray;
-};
-
-export const selectCampsiteById = (id) => (state) => {
-    return state.campsites.campsitesArray.find((campsite) => campsite.id === parseInt(id));
-};
-
-
-export const selectFeaturedCampsite = (state) => {
-    return {
-        featuredItem: state.campsites.campsitesArray.find(
-            (campsite) => campsite.featured
-        ),
-        isLoading: state.campsites.isLoading,
-        errMsg: state.campsites.errMsg
-    }
-
-    // state.campsites.campsitesArray.find((campsite) => campsite.featured);
-};
--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,8 +1,8 @@
 
 import { ScrollView, Text, View } from "react-native";
-import { PARTNERS } from "../shared/partners";
 import { Avatar, Card, ListItem } from 'react-native-elements';
-import { useState } from 'react';
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
 
 const Mission = () => {
     return (
@@ -19,7 +19,7 @@ const Mission = () => {
 
 const AboutScreen = () => {
 
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners)
 
     return (
         <ScrollView>
@@ -29,9 +29,9 @@ const AboutScreen = () => {
                     Community Partners
                 </Card.Title>
                 <Card.Divider />
-                {partners.map(partner =>
+                {partners.partnersArray.map(partner =>
                     <ListItem key={partner.id}>
-                        <Avatar rounded source={partner.image} />
+                        <Avatar rounded source={{ uri: baseUrl + partner.image }} />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
                             <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
