d5a3d4456dd8bedee4d2c842687438d4d10a3e76
Alex Colby <lexprod@gmail.com>
2023-09-23 08:27:56 -0700

    forms

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -16,7 +16,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
-
+import ReservationScreen from './ReservationScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -86,6 +86,29 @@ const ContactNavigator = () => {
         </Stack.Navigator>
     )
 }
+
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Reservation'
+                component={ReservationScreen}
+                options={({ navigation }) => ({
+                    title: 'Reservation Search',
+                    headerLeft: () => (
+                        <Icon
+                            name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
 
 
 
@@ -181,6 +204,22 @@ const Main = () => {
                         />
                     )
                 }} />
+                <Drawer.Screen
+                    name='ReserveCampsite'
+                    component={ReservationNavigator}
+                    options={{
+                        title: 'Reserve Campsite',
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='tree'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
                 <Drawer.Screen name='About' component={AboutNavigator} options={{
                     title: 'About',
                     drawerIcon: ({ color }) => (
